7232a487d2abac76f0d91aa2ed81b78039d3d99d
Trailblaster <66819523+Trailblaster@users.noreply.github.com>
2021-07-11 21:22:48 +1200

    Update Updater.lua

--- Updater.lua
+++ Updater.lua
@@ -1,9 +1,9 @@
 fs.delete("ToasterOSUpdater")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/blob/main/Updater.lua ToasterOSUpdater")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/blob/main/SystemFiles/startup.lua startup.lua")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/blob/main/SystemFiles/UserData/ProgramShortcuts/Shell SystemFiles/UserData/ProgramShortcuts/Shell")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/blob/main/SystemFiles/UserData/ProgramShortcuts/Worm SystemFiles/UserData/ProgramShortcuts/Worm")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/blob/main/SystemFiles/SystemFiles/CreateAppShortcut.lua SystemFiles/CreateAppShortcut.lua")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/blob/main/SystemFiles/SystemFiles/WindowManager.lua SystemFiles/WindowManager.lua")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/Updater.lua ToasterOSUpdater")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/startup.lua startup.lua")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserData/ProgramShortcuts/Shell SystemFiles/UserData/ProgramShortcuts/Shell")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserData/ProgramShortcuts/Worm SystemFiles/UserData/ProgramShortcuts/Worm")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/SystemFiles/CreateAppShortcut.lua SystemFiles/CreateAppShortcut.lua")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/SystemFiles/WindowManager.lua SystemFiles/WindowManager.lua")
 
 --https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/README.md?token=AP5ZLQ3IWSYOLRY3BVGCP4LA5K3Z4
