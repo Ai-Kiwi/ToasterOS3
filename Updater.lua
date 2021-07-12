4d1c588961aab9554ecff625c12781af8e123c49
Trailblaster <66819523+Trailblaster@users.noreply.github.com>
2021-07-13 08:58:10 +1200

    fixed installer

--- Updater.lua
+++ Updater.lua
@@ -1,8 +1,8 @@
 fs.delete("ToasterOSUpdater")
 shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/Updater.lua ToasterOSUpdater")
 shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/startup.lua startup.lua")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserData/ProgramShortcuts/Shell SystemFiles/UserData/ProgramShortcuts/Shell")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserData/ProgramShortcuts/Worm SystemFiles/UserData/ProgramShortcuts/Worm")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserData/ProgramShortcuts/Shell UserData/ProgramShortcuts/Shell")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserData/ProgramShortcuts/Worm UserData/ProgramShortcuts/Worm")
 shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/SystemFiles/CreateAppShortcut.lua SystemFiles/CreateAppShortcut.lua")
 shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/SystemFiles/WindowManager.lua SystemFiles/WindowManager.lua")
 
