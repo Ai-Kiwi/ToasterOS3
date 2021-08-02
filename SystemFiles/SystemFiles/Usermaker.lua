92042a9eb83ba586391d96b578eefadbb0ef08c5
AI Kiwi <66819523+Ai-Kiwi@users.noreply.github.com>
2021-08-02 19:41:26 +1200

    Update Usermaker.lua

--- SystemFiles/SystemFiles/Usermaker.lua
+++ SystemFiles/SystemFiles/Usermaker.lua
@@ -6,4 +6,4 @@ NewUserName = args[1]
 shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserTemplate/ProgramShortcuts/Shell UserData/" .. NewUserName .. "/ProgramShortcuts/Shell")
 shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserTemplate/ProgramShortcuts/Worm UserData/" .. NewUserName .. "/ProgramShortcuts/Worm")
 shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserTemplate/ProgramShortcuts/Settings UserData/" .. NewUserName .. "/ProgramShortcuts/Settings")
-shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserTemplate/ProgramShortcuts/File%20Explorer UserData/" .. NewUserName .. "/ProgramShortcuts/FileExplorer")
+shell.run("wget https://raw.githubusercontent.com/Ai-Kiwi/ToasterOS3/main/SystemFiles/UserTemplate/ProgramShortcuts/FileExplorer UserData/" .. NewUserName .. "/ProgramShortcuts/FileExplorer")
